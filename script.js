52ba59d7dc3df61f18fab5093c4fb3ed9d8dea24
majomenoh <majomarin2006@gmail.com>
2025-04-03 21:40:32 -0500

    un commit

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
 document.getElementById("downloadAndroidBtn").addEventListener("click", function() {
-    const apkUrl = "https://github.com/majomenoh/ejecutable-LM/raw/main/app-release.apk";
-    iniciarDescarga(apkUrl);
+    const apkUrl = "https://drive.google.com/drive/folders/1V1EszV-lgY0fh1kMl4ggIaJZZMOlIM5i"; 
+    window.location.href = apkUrl;
     document.getElementById("message").textContent = "✅ Descarga de la versión para Android iniciada. Revisa tu carpeta de descargas.";
 });
 
 document.getElementById("downloadWindowsBtn").addEventListener("click", function() {
-    const exeUrl = "https://github.com/majomenoh/ejecutable-LM/raw/main/Release.rar";
-    iniciarDescarga(exeUrl);
+    const exeUrl = "https://drive.google.com/drive/folders/1DCWIQM87cN3qwVc_W4FfgTQSNmtJAd9c"; 
+    window.location.href = exeUrl;
     document.getElementById("message").textContent = "✅ Descarga de la versión para Windows iniciada. Revisa tu carpeta de descargas.";
 });
-
-function iniciarDescarga(url) {
-    window.open(url, "_blank"); 
