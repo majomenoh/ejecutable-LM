825ea4aae99a9989c42ebf5df0b49376e1024c1c
majomenoh <majomarin2006@gmail.com>
2025-04-03 19:56:27 -0500

    carga de archivos

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 document.getElementById("downloadAndroidBtn").addEventListener("click", function() {
-    const apkUrl = "C:/Users/Majo/Desktop/paginaAPKLM/apkLM/ANDROID/app-release.apk";
+    const apkUrl = "https://raw.githubusercontent.com/majomenoh/ejecutable-LM/main/app-release.apk"; // URL real del APK
     iniciarDescarga(apkUrl, "LibroMayor.apk");
     document.getElementById("message").textContent = "✅ Descarga de la versión para Android iniciada. Revisa tu carpeta de descargas.";
 });
 
 document.getElementById("downloadWindowsBtn").addEventListener("click", function() {
-    const exeUrl = "C:/Users/Majo/Desktop/paginaAPKLM/apkLM/WINDOWS/libromayor_frontend.exe"; 
-    iniciarDescarga(exeUrl, "LibroMayor.exe");
+    const exeUrl = "https://raw.githubusercontent.com/majomenoh/ejecutable-LM/main/Release.rar"; // URL real del RAR
+    iniciarDescarga(exeUrl, "LibroMayor.rar");
     document.getElementById("message").textContent = "✅ Descarga de la versión para Windows iniciada. Revisa tu carpeta de descargas.";
 });
 
 function iniciarDescarga(url, nombreArchivo) {
     let link = document.createElement("a");
     link.href = url;
-    link.download = nombreArchivo;
+    link.setAttribute("download", nombreArchivo);
+    link.setAttribute("target", "_blank");
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
